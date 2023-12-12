fb28ad1cc5dc5b54f5ce966e340fa24a14277a0b
Ning39 <hkym39@outlook.com>
2023-12-12 16:48:34 +0800

    初始化工程

---
+++ src/settings.ts
@@ -0,0 +1,16 @@
+const defaultSettings: AppSettings = {
+  title: "黑马人力资源后台管理系统", // 网站标题
+  version: "v0.0.1", // 版本控制
+  showSettings: true, //展示设置
+  tagsView: true,
+  fixedHeader: false, // 固定头部栏
+  sidebarLogo: true, // 侧边栏Logo
+  layout: "left",
+  theme: "light",
+  size: "default",
+  language: "zh-cn",
+  themeColor: "#409EFF",
+  watermark: { enabled: false, content: "测试" },
+};
+
+export default defaultSettings;
